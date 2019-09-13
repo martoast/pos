117f38cc0db2d126af0608cb61c4707fe8fb926f
Alex Martos <alexmartos@gmail.com>
2019-09-13 13:35:02 -0700

    second push

--- layouts/default.vue
+++ layouts/default.vue
@@ -64,16 +64,7 @@
         </template>
       </v-navigation-drawer>
     </v-card>
-    <!-- <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>mdi-repeat</v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>-->
+
     <v-footer :fixed="fixed" app>
       <span>&copy; 2019</span>
     </v-footer>
