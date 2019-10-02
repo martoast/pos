c29df027d46bd92a723f8dc240e60152492c7393
dev <alexmartos96@gmail.com>
2019-10-01 18:07:33 -0700

    settings card width

--- pages/Settings/Display.vue
+++ pages/Settings/Display.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <v-card width="950">
+    <v-card
+      class="mx-auto"
+      max-width="800"
+    >
       <v-container fluid>
         <h2>Brightness</h2>
         <v-row>
@@ -21,30 +24,74 @@
         <h2>Language</h2>
         <h3>{{language}}</h3>
         <v-radio-group column>
-          <v-radio v-model="language" label="English" value="english"></v-radio>
-          <v-radio v-model="language" label="Spanish" value="spanish"></v-radio>
-          <v-radio v-model="language" label="Korean" value="korean"></v-radio>
+          <v-radio
+            v-model="language"
+            label="English"
+            value="english"
+          ></v-radio>
+          <v-radio
+            v-model="language"
+            label="Spanish"
+            value="spanish"
+          ></v-radio>
+          <v-radio
+            v-model="language"
+            label="Korean"
+            value="korean"
+          ></v-radio>
         </v-radio-group>
         <hr />
         <h2>Font</h2>
         <v-radio-group column>
-          <v-radio v-model="font" label="Small" value="small"></v-radio>
-          <v-radio v-model="font" label="Medium" value="medium"></v-radio>
-          <v-radio v-model="font" label="Large" value="large"></v-radio>
+          <v-radio
+            v-model="font"
+            label="Small"
+            value="small"
+          ></v-radio>
+          <v-radio
+            v-model="font"
+            label="Medium"
+            value="medium"
+          ></v-radio>
+          <v-radio
+            v-model="font"
+            label="Large"
+            value="large"
+          ></v-radio>
         </v-radio-group>
         <hr />
         <h2>Time Displayed</h2>
         <v-radio-group>
-          <v-radio v-model="timedisplay" label="24-hr Format" value="24"></v-radio>
-          <v-radio v-model="timedisplay" label="12- hr Format" value="12"></v-radio>
+          <v-radio
+            v-model="timedisplay"
+            label="24-hr Format"
+            value="24"
+          ></v-radio>
+          <v-radio
+            v-model="timedisplay"
+            label="12- hr Format"
+            value="12"
+          ></v-radio>
         </v-radio-group>
       </v-container>
       <hr />
       <h2>Items Displayed</h2>
       <v-radio-group column>
-        <v-radio v-model="displayitems" label="20" value="20"></v-radio>
-        <v-radio v-model="displayitems" label="40" value="40"></v-radio>
-        <v-radio v-model="displayitems" label="50" value="50"></v-radio>
+        <v-radio
+          v-model="displayitems"
+          label="20"
+          value="20"
+        ></v-radio>
+        <v-radio
+          v-model="displayitems"
+          label="40"
+          value="40"
+        ></v-radio>
+        <v-radio
+          v-model="displayitems"
+          label="50"
+          value="50"
+        ></v-radio>
       </v-radio-group>
     </v-card>
   </div>
--- pages/Settings/PaymentOption.vue
+++ pages/Settings/PaymentOption.vue
@@ -1,32 +1,83 @@
 <template>
   <div>
-    <v-card class="mx-auto">
+    <v-card
+      class="mx-auto"
+      max-width="800"
+    >
       <v-container fluid>
         <h2>Accepted Payment Methods</h2>
         <p>{{ selected }}</p>
-        <v-checkbox v-model="selected" label="Credit" value="credit"></v-checkbox>
-        <v-checkbox v-model="selected" label="Debit" value="debit"></v-checkbox>
-        <v-checkbox v-model="selected" label="Cash" value="cash"></v-checkbox>
-        <v-checkbox v-model="selected" label="Coupon" value="coupon"></v-checkbox>
-        <v-checkbox v-model="selected" label="Gift Card" value="giftcard"></v-checkbox>
+        <v-checkbox
+          v-model="selected"
+          label="Credit"
+          value="credit"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="selected"
+          label="Debit"
+          value="debit"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="selected"
+          label="Cash"
+          value="cash"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="selected"
+          label="Coupon"
+          value="coupon"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="selected"
+          label="Gift Card"
+          value="giftcard"
+        ></v-checkbox>
       </v-container>
       <hr />
       <h2>Receipt Signature Threshold</h2>
       <p>Minimum Signature Amount</p>
       <p>${{ minsignature }}</p>
       <v-container>
-        <v-col cols="12" sm="10" md="10">
-          <v-text-field v-model="minsignature" placeholder="$5.00" outlined></v-text-field>
+        <v-col
+          cols="12"
+          sm="10"
+          md="10"
+        >
+          <v-text-field
+            v-model="minsignature"
+            placeholder="$5.00"
+            outlined
+          ></v-text-field>
         </v-col>
       </v-container>
       <v-container fluid>
         <h2>Tip Amounts</h2>
         <p>{{ tip_amounts }}</p>
-        <v-checkbox v-model="tip_amounts" label="%5" value="5"></v-checkbox>
-        <v-checkbox v-model="tip_amounts" label="%10" value="10"></v-checkbox>
-        <v-checkbox v-model="tip_amounts" label="%15" value="15"></v-checkbox>
-        <v-checkbox v-model="tip_amounts" label="%20" value="20"></v-checkbox>
-        <v-checkbox v-model="selected" label="Gift Card" value="giftcard"></v-checkbox>
+        <v-checkbox
+          v-model="tip_amounts"
+          label="%5"
+          value="5"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="tip_amounts"
+          label="%10"
+          value="10"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="tip_amounts"
+          label="%15"
+          value="15"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="tip_amounts"
+          label="%20"
+          value="20"
+        ></v-checkbox>
+        <v-checkbox
+          v-model="selected"
+          label="Gift Card"
+          value="giftcard"
+        ></v-checkbox>
       </v-container>
     </v-card>
   </div>
