860a5c6450ed1cdf87d315b8c4c70095c3ed9473
dev <alexmartos96@gmail.com>
2019-10-17 18:32:40 -0700

    Separated Modal from receipt

--- components/CheckOutModal.vue
+++ components/CheckOutModal.vue
@@ -1,122 +1,183 @@
 <template>
-  <v-row justify="center">
-    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
-      <v-card>
-        <v-card flat>
-          <v-toolbar color="secondary" dark extended flat>
-            <v-btn icon dark @click="dialog = false">
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-            <v-toolbar-title>CheckOut</v-toolbar-title>
-          </v-toolbar>
-          <v-row>
-            <v-card class="mx-auto" max-width="800" style="margin-top: -64px;"></v-card>
-          </v-row>
+  <div>
+    <v-btn
+      height="50"
+      block
+      color="success"
+      @click.stop="dialog = true"
+    >CheckOut</v-btn>
 
-          <v-card class="mx-auto" max-width="800" style="margin-top: 64px;">
-            <v-tabs fixed-tabs background-color="indigo" dark height="64" v-model="tab">
-              <v-tab>Pay with Cash</v-tab>
-              <v-tab>Pay with Card</v-tab>
-              <v-tabs-items v-model="tab">
-                <v-tab-item>
-                  <v-card elevation="12">
-                    <h3 class="center">Choose Amount</h3>
-                    <v-row class="center">
-                      <v-btn
-                        x-large
-                        class="ma-2"
-                        tile
-                        outlined
-                        color="success"
-                        v-on:click="PaidAmount += 1"
-                      >$1.00</v-btn>
-                      <v-btn
-                        x-large
-                        class="ma-2"
-                        tile
-                        outlined
-                        color="success"
-                        v-on:click="PaidAmount += 5"
-                      >$5.00</v-btn>
-                      <v-btn
-                        x-large
-                        class="ma-2"
-                        tile
-                        outlined
-                        color="success"
-                        v-on:click="PaidAmount += 10"
-                      >$10.00</v-btn>
-                      <v-btn
-                        x-large
-                        class="ma-2"
-                        tile
-                        outlined
-                        color="success"
-                        v-on:click="PaidAmount += 20"
-                      >$20.00</v-btn>
-                      <v-btn
-                        x-large
-                        class="ma-2"
-                        tile
-                        outlined
-                        color="success"
-                        v-on:click="PaidAmount += CartTotal"
-                      >Exact</v-btn>
-                    </v-row>
-                    <v-row class="center">
-                      <v-col cols="12" sm="6">
-                        <h1>{{PaidAmount}}</h1>
-                        <h4>change due: {{ChangeDue}}</h4>
-                        <v-row>
-                          <v-text-field outlined></v-text-field>
-                          <div class="text-center">
-                            <v-btn class="display" @click="DeletePaidAmount">Delete</v-btn>
-                          </div>
-                        </v-row>
-                      </v-col>
-                    </v-row>
-                    <div>
-                      <v-card-actions>
-                        <v-spacer />
+    <v-row justify="center">
+      <v-dialog
+        v-model="dialog"
+        fullscreen
+        hide-overlay
+        transition="dialog-bottom-transition"
+      >
+        <v-card>
+          <v-card flat>
+            <v-toolbar
+              color="secondary"
+              dark
+              extended
+              flat
+            >
+              <v-btn
+                icon
+                dark
+                @click="dialog = false"
+              >
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+              <v-toolbar-title>CheckOut</v-toolbar-title>
+            </v-toolbar>
+            <v-row>
+              <v-card
+                class="mx-auto"
+                max-width="800"
+                style="margin-top: -64px;"
+              ></v-card>
+            </v-row>
 
-                        <v-btn x-large right color="success" dark @click="dialog = false">Pay Now</v-btn>
-                      </v-card-actions>
-                    </div>
-                  </v-card>
-                </v-tab-item>
-                <v-tab-item>
-                  <v-card-text style="height: 50px;"></v-card-text>
-                  <v-card>
-                    <v-text-field label="Sign here*"></v-text-field>
-                    <small>*indicates required field</small>
-                    <div class="my-2">
-                      <v-btn x-large color="error" dark>Clear Sinature</v-btn>
-                      <v-btn x-large color="success" dark @click="dialog = false">Done Signing</v-btn>
-                    </div>
-                  </v-card>
-                </v-tab-item>
-              </v-tabs-items>
-            </v-tabs>
+            <v-card
+              class="mx-auto"
+              max-width="800"
+              style="margin-top: 64px;"
+            >
+              <v-tabs
+                fixed-tabs
+                background-color="indigo"
+                dark
+                height="64"
+                v-model="tab"
+              >
+                <v-tab>Pay with Cash</v-tab>
+                <v-tab>Pay with Card</v-tab>
+                <v-tabs-items v-model="tab">
+                  <v-tab-item>
+                    <v-card elevation="12">
+                      <h3 class="center">Choose Amount</h3>
+                      <v-row class="center">
+                        <v-btn
+                          x-large
+                          class="ma-2"
+                          tile
+                          outlined
+                          color="success"
+                          v-on:click="PaidAmount += 1"
+                        >$1.00</v-btn>
+                        <v-btn
+                          x-large
+                          class="ma-2"
+                          tile
+                          outlined
+                          color="success"
+                          v-on:click="PaidAmount += 5"
+                        >$5.00</v-btn>
+                        <v-btn
+                          x-large
+                          class="ma-2"
+                          tile
+                          outlined
+                          color="success"
+                          v-on:click="PaidAmount += 10"
+                        >$10.00</v-btn>
+                        <v-btn
+                          x-large
+                          class="ma-2"
+                          tile
+                          outlined
+                          color="success"
+                          v-on:click="PaidAmount += 20"
+                        >$20.00</v-btn>
+                        <v-btn
+                          x-large
+                          class="ma-2"
+                          tile
+                          outlined
+                          color="success"
+                          v-on:click="PaidAmount += CartTotal"
+                        >Exact</v-btn>
+                      </v-row>
+                      <v-row class="center">
+                        <v-col
+                          cols="12"
+                          sm="6"
+                        >
+                          <h1>{{PaidAmount}}</h1>
 
-            <v-divider></v-divider>
+                          <v-row>
+                            <v-text-field outlined></v-text-field>
+                            <div class="text-center">
+                              <v-btn
+                                class="display"
+                                @click="DeletePaidAmount"
+                              >Delete</v-btn>
+                            </div>
+                          </v-row>
+                        </v-col>
+                      </v-row>
+                      <div>
+                        <v-card-actions>
+                          <v-spacer />
+
+                          <v-btn
+                            x-large
+                            right
+                            color="success"
+                            dark
+                            @click="dialog = false"
+                          >Pay Now</v-btn>
+                        </v-card-actions>
+                      </div>
+                    </v-card>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-card-text style="height: 50px;"></v-card-text>
+                    <v-card>
+                      <v-text-field label="Sign here*"></v-text-field>
+                      <small>*indicates required field</small>
+                      <div class="my-2">
+                        <v-btn
+                          x-large
+                          color="error"
+                          dark
+                        >Clear Sinature</v-btn>
+                        <v-btn
+                          x-large
+                          color="success"
+                          dark
+                          @click="dialog=false"
+                        >Done Signing</v-btn>
+                      </div>
+                    </v-card>
+                  </v-tab-item>
+                </v-tabs-items>
+              </v-tabs>
+
+              <v-divider></v-divider>
+            </v-card>
           </v-card>
         </v-card>
-      </v-card>
-    </v-dialog>
-  </v-row>
+      </v-dialog>
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      PaidAmount: null,
+      tab: null,
       dialog: false
     };
   },
   methods: {
     DeletePaidAmount() {
       this.PaidAmount = null;
-    }
+    },
+    CloseDialog() {}
   }
 };
 </script>
--- components/MainReceiptPreview.vue
+++ components/MainReceiptPreview.vue
@@ -61,10 +61,10 @@
 
           <tr
             v-for="item in MenuItems"
-            :key="item.FoodModifiers.name"
+            :key="item.name"
           >
-            <td>{{item.FoodModifiers.name}}</td>
-            <td>{{ item.FoodModifiers.price }}</td>
+            <td>{{item.name}}</td>
+            <td>{{ item.price }}</td>
           </tr>
         </tbody>
 
@@ -77,177 +77,24 @@
 
     </v-card>
 
-    <v-btn
-      height="50"
-      block
-      color="success"
-      @click.stop="dialog = true"
-    >CheckOut</v-btn>
-    <template>
+    <CheckOutModal />
 
-      <v-dialog
-        v-model="dialog"
-        fullscreen
-        transition="dialog-bottom-transition"
-      >
-        <v-card>
-          <v-card flat>
-            <v-toolbar
-              color="secondary"
-              dark
-              extended
-              flat
-            >
-              <v-btn
-                icon
-                dark
-                @click="dialog = false"
-              >
-                <v-icon>mdi-close</v-icon>
-              </v-btn>
-              <v-toolbar-title>CheckOut</v-toolbar-title>
-            </v-toolbar>
-
-            <v-card
-              class="mx-auto"
-              max-width="800"
-              style="margin-top: 64px;"
-            >
-              <v-tabs
-                fixed-tabs
-                background-color="indigo"
-                dark
-                height="64"
-                v-model="tab"
-              >
-                <v-tab>Pay with Cash</v-tab>
-                <v-tab>Pay with Card</v-tab>
-                <v-tabs-items v-model="tab">
-                  <v-tab-item>
-                    <v-card elevation="12">
-                      <div>
-                        <h3>Choose Amount</h3>
-                        <v-row>
-                          <v-btn
-                            x-large
-                            class="ma-2"
-                            tile
-                            outlined
-                            color="success"
-                            v-on:click="PaidAmount += 1"
-                          >$1.00</v-btn>
-                          <v-btn
-                            x-large
-                            class="ma-2"
-                            tile
-                            outlined
-                            color="success"
-                            v-on:click="PaidAmount += 5"
-                          >$5.00</v-btn>
-                          <v-btn
-                            x-large
-                            class="ma-2"
-                            tile
-                            outlined
-                            color="success"
-                            v-on:click="PaidAmount += 10"
-                          >$10.00</v-btn>
-                          <v-btn
-                            x-large
-                            class="ma-2"
-                            tile
-                            outlined
-                            color="success"
-                            v-on:click="PaidAmount += 20"
-                          >$20.00</v-btn>
-                          <v-btn
-                            x-large
-                            class="ma-2"
-                            tile
-                            outlined
-                            color="success"
-                            v-on:click="PaidAmount += CartTotal"
-                          >Exact</v-btn>
-                        </v-row>
-
-                        <v-col
-                          cols="12"
-                          sm="6"
-                        >
-                          <h1>{{PaidAmount}}</h1>
-                          <h4>change due: {{ChangeDue}}</h4>
-                          <v-row>
-                            <v-text-field outlined></v-text-field>
-                            <div class="text-center">
-                              <v-btn
-                                class="display"
-                                @click="DeletePaidAmount"
-                              >Delete</v-btn>
-                            </div>
-                          </v-row>
-                        </v-col>
-
-                        <div>
-                          <v-card-actions>
-                            <v-spacer />
-
-                            <v-btn
-                              x-large
-                              right
-                              color="success"
-                              dark
-                              @click="dialog = false"
-                            >Pay Now</v-btn>
-                          </v-card-actions>
-                        </div>
-                      </div>
-
-                    </v-card>
-                  </v-tab-item>
-                  <v-tab-item>
-                    <v-card-text style="height: 50px;"></v-card-text>
-                    <v-card>
-                      <v-text-field label="Sign here*"></v-text-field>
-                      <small>*indicates required field</small>
-                      <div class="my-2">
-                        <v-btn
-                          x-large
-                          color="error"
-                          dark
-                        >Clear Sinature</v-btn>
-                        <v-btn
-                          x-large
-                          color="success"
-                          dark
-                          @click="dialog = false"
-                        >Done Signing</v-btn>
-                      </div>
-                    </v-card>
-                  </v-tab-item>
-                </v-tabs-items>
-              </v-tabs>
-
-              <v-divider></v-divider>
-            </v-card>
-          </v-card>
-        </v-card>
-      </v-dialog>
-
-    </template>
   </div>
 </template>
 
 
 <script>
+import CheckOutModal from "~/components/CheckOutModal.vue";
 export default {
+  components: {
+    CheckOutModal
+  },
   data() {
     return {
-      MenuItems: [],
-      FoodModifiers: [],
+      MenuItems: null,
+
       Total: [],
-      dialog: false,
-      tab: null,
-      PaidAmount: null,
+
       OrderType: "Dine-In",
       icon: "mdi-food-fork-drink",
 
@@ -261,26 +108,23 @@ export default {
   created() {
     this.$nuxt.$on("test", data => {
       // this.total = data;
-      this.MenuItems.push(data);
+      this.MenuItems = data;
       console.log(this.MenuItems);
     });
   },
   methods: {
     DeleteItem(item) {
-      this.MenuItems.splice(item, 1);
-      this.Total.splice(item, 1);
+      // this.MenuItems.splice(item, 1);
+      // this.Total.splice(item, 1);
     },
     DeleteModifier(modifier) {
-      this.FoodModifiers.splice(modifier, 1);
-      this.Total.splice(modifier, 1);
+      // this.MenuItems.splice(item, 1);
+      // this.Total.splice(item, 1);
     },
     selected(option) {
       console.log(option.title);
       this.OrderType = option.title;
       this.icon = option.icon;
-    },
-    DeletePaidAmount() {
-      this.PaidAmount = null;
     }
   },
   computed: {
