45abe5baf5be2aa349f972b9a61d9baa5b36c128
Alex Martos <alexmartos@gmail.com>
2019-12-21 15:45:03 -0800

    modifiers added to edit modal

--- components/MenuConfigTable.vue
+++ components/MenuConfigTable.vue
@@ -24,75 +24,96 @@
                 </v-btn>
               </v-row>
             </div>
+            <v-dialog v-model="dialog" max-width="600px">
+              <v-row justify="center">
+                <v-form v-model="valid" lazy-validation>
+                  <v-card>
+                    <v-card-title>
+                      <span class="headline">edit {{item.name}} item</span>
+                    </v-card-title>
+                    <v-card-text>
+                      <v-container>
+                        <v-row justify="center">
+                          <v-col cols="12" sm="6" md="4">
+                            <v-text-field
+                              label="Item name*"
+                              v-model="item.name"
+                              :rules="nameRules"
+                              color="secondary"
+                              required
+                            ></v-text-field>
+                            <v-text-field
+                              label="Price*"
+                              v-model="item.price"
+                              :rules="priceRules"
+                              color="secondary"
+                              required
+                            ></v-text-field>
+                          </v-col>
+
+                          <v-col cols="12" sm="6">
+                            <v-select
+                              :items="['Food', 'Drink', 'Dessert']"
+                              label="Type*"
+                              v-model="item.type"
+                              required
+                            ></v-select>
+                            <v-select
+                              v-model="item.sizes"
+                              :items="['Small', 'Medium', 'Large', 'Half', 'Full']"
+                              :rules="sizeRules"
+                              required
+                              chips
+                              label="Sizes"
+                              multiple
+                            ></v-select>
+                          </v-col>
+                          <!-- <v-col cols="12" sm="6"></v-col> -->
+
+                          <v-card
+                            class="mx-auto"
+                            max-width="300"
+                            tile
+                            v-if="item.modifiers.length > 0"
+                          >
+                            <v-list rounded>
+                              <v-subheader>
+                                <v-row justify="center" align="center">
+                                  <v-card-text>Item Modifiers</v-card-text>
+                                </v-row>
+                              </v-subheader>
+                              <v-list-item-group color="primary">
+                                <v-list-item v-for="(item, i) in item.modifiers" :key="i">
+                                  <v-list-item-content @click="DeleteModifier(item,i)">
+                                    <v-list-item-title v-text="item.name"></v-list-item-title>
+                                  </v-list-item-content>
+                                </v-list-item>
+                              </v-list-item-group>
+                            </v-list>
+                            <v-btn color="secondary" dark @click="dialog3 = true">Edit Modifier</v-btn>
+                          </v-card>
+                        </v-row>
+                      </v-container>
+                      <small>*indicates required field</small>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+                      <v-btn
+                        :disabled="!valid"
+                        color="secondary"
+                        :loading="loading"
+                        @click="SaveItem()"
+                      >Save</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-form>
+              </v-row>
+            </v-dialog>
           </td>
         </tr>
       </tbody>
     </v-simple-table>
-
-    <v-dialog v-model="dialog" max-width="600px">
-      <v-row justify="center">
-        <v-form v-model="valid" lazy-validation>
-          <v-card>
-            <v-card-title>
-              <span class="headline">edit {{item.name}} item</span>
-            </v-card-title>
-            <v-card-text>
-              <v-container>
-                <v-row justify="center">
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      label="Item name*"
-                      v-model="item.name"
-                      :rules="nameRules"
-                      color="secondary"
-                      required
-                    ></v-text-field>
-                    <v-text-field
-                      label="Price*"
-                      v-model="item.price"
-                      :rules="priceRules"
-                      color="secondary"
-                      required
-                    ></v-text-field>
-                  </v-col>
-
-                  <v-col cols="12" sm="6">
-                    <v-select
-                      :items="['Food', 'Drink', 'Dessert']"
-                      label="Type*"
-                      v-model="item.type"
-                      required
-                    ></v-select>
-                    <v-select
-                      v-model="item.sizes"
-                      :items="['Small', 'Medium', 'Large', 'Half', 'Full']"
-                      :rules="sizeRules"
-                      required
-                      chips
-                      label="Sizes"
-                      multiple
-                    ></v-select>
-                  </v-col>
-                  <v-col cols="12" sm="6"></v-col>
-                  <v-btn color="secondary" dark @click="dialog3 = true">Edit Modifier</v-btn>
-                </v-row>
-              </v-container>
-              <small>*indicates required field</small>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-              <v-btn
-                :disabled="!valid"
-                color="secondary"
-                :loading="loading"
-                @click="SaveItem()"
-              >Save</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-form>
-      </v-row>
-    </v-dialog>
   </div>
 </template>
 <script>
@@ -119,15 +140,9 @@ export default {
   },
   created() {
     this.$store.dispatch("user/GET_EMAIL");
-    this.$store.dispatch("menu/GET_FIREMENU", this.email);
   },
-  mounted() {
-    // console.log(this.email);
-  },
+
   computed: {
-    // MenuItems() {
-    //   return this.$store.getters["menu/getMenu"];
-    // },
     MenuItems: {
       get() {
         return this.$store.getters["menu/getMenu"];
@@ -140,11 +155,17 @@ export default {
       return this.$store.getters["user/EmailGetter"];
     }
   },
-  mounted() {},
+  mounted() {
+    this.$store.dispatch("menu/GET_FIREMENU", this.email);
+  },
   methods: {
     DeleteItem(item) {
       console.log("deleting item...");
       this.$store.commit("menu/remove", item);
+    },
+    DeleteModifier(item) {
+      console.log(item);
+      console.log("Modifier Deleted");
     },
     SaveEdit() {
       this.dialog = false;
