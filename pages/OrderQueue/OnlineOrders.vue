70bfdf9427581235f57e80b5e21b86a1e886a080
dev <alexmartos96@gmail.com>
2020-01-02 15:32:11 -0800

    pagination and search feature on Orderqueue

--- pages/OrderQueue/OnlineOrders.vue
+++ pages/OrderQueue/OnlineOrders.vue
@@ -1,22 +1,44 @@
 
 <template>
   <div>
-
     <v-container fluid>
       <v-data-iterator
         :items="items"
         :items-per-page.sync="itemsPerPage"
-        :page="page"
+        :footer-props="{ itemsPerPageOptions }"
         :search="search"
-        :sort-by="sortBy.toLowerCase()"
-        :sort-desc="sortDesc"
-        hide-default-footer
       >
+        <template v-slot:header>
+          <v-toolbar
+            dark
+            color="secondary"
+          >
+            <v-text-field
+              v-model="search"
+              clearable
+              flat
+              solo-inverted
+              hide-details
+              label="Search"
+            ></v-text-field>
+            <template v-if="$vuetify.breakpoint.mdAndUp">
+              <v-spacer></v-spacer>
+              <v-select
+                v-model="sortBy"
+                flat
+                solo-inverted
+                hide-details
+                :items="keys"
+                label="Sort by"
+              ></v-select>
 
+            </template>
+          </v-toolbar>
+        </template>
         <template v-slot:default="props">
-          <v-row class="ma-10 pa-10">
+          <v-row>
             <v-col
-              v-for="item in items"
+              v-for="item in props.items"
               :key="item.id"
               cols="12"
               sm="6"
@@ -31,16 +53,6 @@
 
                 <v-divider></v-divider>
 
-                <!-- <v-list dense>
-                  <v-list-item
-                    v-for="(key, index) in filteredKeys"
-                    :key="index"
-                    :color="sortBy === key ? `blue lighten-4` : `white`"
-                  >
-                    <v-list-item-content>{{ key }}:</v-list-item-content>
-                    <v-list-item-content class="align-end">{{ item[key.toLowerCase()] }}</v-list-item-content>
-                  </v-list-item>
-                </v-list> -->
                 <v-simple-table>
 
                   <tbody>
@@ -67,65 +79,8 @@
                 </v-simple-table>
                 <v-divider></v-divider>
                 <v-card-title class="subheading font-weight-bold">Total: ${{ item.OrderTotal }}</v-card-title>
-
               </v-card>
             </v-col>
-          </v-row>
-
-        </template>
-
-        <template v-slot:footer>
-          <v-row
-            class="mt-2"
-            align="center"
-            justify="center"
-          >
-
-            <v-menu offset-y>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  dark
-                  text
-                  color="primary"
-                  class="ml-2"
-                  v-on="on"
-                >
-                  {{ itemsPerPage }}
-                  <v-icon>mdi-chevron-down</v-icon>
-                </v-btn>
-              </template>
-              <v-list>
-                <v-list-item
-                  v-for="(number, index) in itemsPerPageArray"
-                  :key="index"
-                  @click="updateItemsPerPage(number)"
-                >
-                  <v-list-item-title>{{ number }}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-
-            <div class="flex-grow-1"></div>
-
-            <span class="mr-4 grey--text">Page {{ page }} of {{ numberOfPages }}</span>
-            <v-btn
-              fab
-              dark
-              color="blue darken-3"
-              class="mr-1"
-              @click="formerPage"
-            >
-              <v-icon>mdi-chevron-left</v-icon>
-            </v-btn>
-            <v-btn
-              fab
-              dark
-              color="blue darken-3"
-              class="ml-1"
-              @click="nextPage"
-            >
-              <v-icon>mdi-chevron-right</v-icon>
-            </v-btn>
           </v-row>
         </template>
       </v-data-iterator>
@@ -153,21 +108,23 @@
       </v-dialog>
     </div>
   </div>
+
 </template>
 <script>
 export default {
   layout: "OrderQueue",
   data() {
     return {
-      itemsPerPageArray: [4, 8, 12],
+      itemsPerPageOptions: [4, 8, 12],
       dialog: false,
       search: "",
       filter: {},
-      sortDesc: false,
+
       page: 1,
       itemsPerPage: 4,
+      keys: ["Cash", "Card"],
       sortBy: "name",
-      keys: ["Pizza", "Soda", "Salad", "Name", "Tax", "ID"],
+
       items: []
     };
   },
